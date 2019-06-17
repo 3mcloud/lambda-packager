730bcb121e5366ec0b1dc76aca5ef8703e83fc85
Kevin Truckenmiller <ktruckenmiller@mmm.com>
2019-06-17 10:47:40 -0500

    updated with initial tests

--- tests/python/3.6/src/lambda_function.py
+++ tests/python/3.6/src/lambda_function.py
@@ -2,5 +2,5 @@ import requests
 
 
 def lamda_handler(event, context):
-    r = requests.get('https://google.com')
+    r = requests.get('https://www.3m.com')
     return "hello world, " + r.status_code
