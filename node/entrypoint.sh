604ee664fd451a15dfc409372ea2dc1b1f0e292c
Peter St.Jean <pstjean@mmm.com>
2020-02-18 08:44:29 -0600

    Node entrypoint script

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -5,20 +5,16 @@ CODE=${LAMBDA_CODE_DIR:-"src"}
 ARTIFACT=${ARTIFACT_NAME:-"deployment.zip"}
 BUILD_DIR=${CONTAINER_BUILD_DIRECTORY:-"/build"}
 WORKSPACE=${CI_WORKSPACE:-$(pwd)}
-REQUIREMENTS_FILE=${REQUIREMENTS_FILE:-"requirements.txt"}
-SETUP_FILE=${SETUP_FILE:-"setup.py"}
-
 
 ## Actually doing things
 mkdir -p ${BUILD_DIR}
 cp -R ${WORKSPACE}/${CODE}/. ${BUILD_DIR}
 cd ${BUILD_DIR}
-if [ -a ${SETUP_FILE} ]; then
-    pip install -t ./ .
-elif [ -e ${REQUIREMENTS_FILE} ]; then
-    pip install -r ${REQUIREMENTS_FILE} -t ./
+if [ -e "package.json" ]; then
+    npm install
 else
     echo "No requirements found. Skipping install"
 fi
 chmod -R 755 .
-zip -r9 ${WORKSPACE}/${ARTIFACT} . -x "*.pyc" -x ${REQUIREMENTS_FILE} -x "${ARTIFACT}"
+zip -r9 ${WORKSPACE}/${ARTIFACT} . -x ${REQUIREMENTS_FILE} -x "${ARTIFACT}"
+
