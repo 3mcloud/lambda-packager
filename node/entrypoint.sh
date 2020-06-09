50be6585d51f88f64e0b5eeb566f6d39f281cbb8
Nathan Good <nathan.a.good@gmail.com>
2020-06-09 17:18:54 -0500

    Updated entrypoint.sh script for some additional node goodness.

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -5,16 +5,19 @@ CODE=${LAMBDA_CODE_DIR:-"src"}
 ARTIFACT=${ARTIFACT_NAME:-"deployment.zip"}
 BUILD_DIR=${CONTAINER_BUILD_DIRECTORY:-"/build"}
 WORKSPACE=${CI_WORKSPACE:-$(pwd)}
+PKG_FILE=${NPM_PACKAGE_FILE:-"package.json"}
+PKG_LOCK=${NPM_PACKAGE_LOCK:-"package-lock.json"}
 
 ## Actually doing things
-mkdir -p ${BUILD_DIR}
-cp -R ${WORKSPACE}/${CODE}/. ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/${CODE}
+cp -R ${WORKSPACE}/${CODE}/. ${BUILD_DIR}/${CODE}
+cp ${WORKSPACE}/${PKG_FILE} ${BUILD_DIR}
+cp ${WORKSPACE}/${PKG_LOCK} ${BUILD_DIR}
 cd ${BUILD_DIR}
-if [ -e "package.json" ]; then
+if [[ -f "${PKG_FILE}" ]]; then
     npm install --production
 else
     echo "No requirements found. Skipping install"
 fi
-chmod -R 755 .
-zip -r9 ${WORKSPACE}/${ARTIFACT} . -x ${REQUIREMENTS_FILE} -x "${ARTIFACT}"
 
+zip -r9 ${WORKSPACE}/${ARTIFACT} . -x "${PKG_FILE}" -x "${PKG_LOCK}"
