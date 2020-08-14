29a8f89c0cb9d6b296c0e1a8ad60be7799b6d737
Alex Guckenberger <a8l5wzz@mmm.com>
2020-08-14 09:57:44 -0500

    Update glob test.

--- python/tests/test_entrypoint.py
+++ python/tests/test_entrypoint.py
@@ -3,6 +3,7 @@
 # Built Ins
 import os
 import zipfile
+import glob
 
 # 3rd Party
 import pytest
@@ -23,11 +24,27 @@ def test_valid_zips(lambda_paths, environments, context_modified_environ, monkey
             zip_file_name = os.getenv('ARTIFACT_NAME', 'deployment.zip')
             workspace = os.getenv('CI_WORKSPACE', os.getcwd())
             code_dir = os.getenv('LAMBDA_CODE_DIR', 'src')
+            glob_ignore = os.getenv('GLOB_IGNORE', "*.pyc,__pycache__")
+            build_dir = os.getenv('CONTAINER_BUILD_DIRECTORY', '/build')
 
             with zipfile.ZipFile(os.path.join(workspace, zip_file_name)) as f:
                 root_level_files = {e.split('/')[0] for e in f.namelist()}
                 # sorted_file_names = list(sorted(root_level_files))
                 # print("Root level zipped: ", sorted_file_names)
+            
+            for expr in glob_ignore.split(','):
+                full_expr = os.path.join(
+                    workspace,
+                    build_dir + '_clean',
+                    '**',
+                    glob_ignore.split(',')[0]
+                )
+                for filename in glob.iglob(full_expr, recursive=True):
+                    print(
+                        "File that should be ignored ended up in the clean build directory:", filename
+                    )
+                    assert False
+
             assert len(root_level_files) > 10 # Make sure the root level has more then 10 files
             
             _, directories, files = next(os.walk(os.path.join(workspace, code_dir)))
