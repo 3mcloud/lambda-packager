43d4043272eb0e95ac4b23015c97abf69a84415a
Eric Walker <ewalker3@mmm.com>
2021-11-12 14:00:46 -0600

    Correct install

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -2,6 +2,6 @@
 # This file remains for backeards compatiability.
 set -ex
 # Add poetry
-export PATH="/drone/src/.poetry/bin:$PATH"
+export PATH="/root/.local/bin:$PATH"
 python3 /entrypoint.py
 wait
